8e765acf5b6a3e0ab2ceb8d012aeb2d10a0bbcac
tmtocb <kittz.esport@gmail.com>
2021-02-22 14:21:15 +0100

    link GraphQL with React via Apollo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,27 @@ import ReactDOM from 'react-dom';
 import './tailwind.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const link = createHttpLink({
+  uri: 'http://localhost:3000/graphql'
+})
+
+const client = new ApolloClient({
+  link: link,
+  cache: new InMemoryCache()
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

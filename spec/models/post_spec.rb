2d486aca2753dc2fd3f60e0fef208e631193b3d6
tmtocb <kittz.esport@gmail.com>
2021-02-22 12:15:03 +0100

    add models association, follow TDD

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,8 +3,12 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
 
   describe '#columns' do
-    it { is_expected.to have_db_column(:user_id) }
-    it { is_expected.to have_db_column(:title) }
-    it { is_expected.to have_db_column(:body) }
+    it { should have_db_column(:user_id) }
+    it { should have_db_column(:title) }
+    it { should have_db_column(:body) }
+  end
+
+  describe '#associations' do
+    it { should belong_to(:user) }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   describe '#columns' do
-    it { is_expected.to have_db_column(:email) }
-    it { is_expected.to have_db_column(:name) }
+    it { should have_db_column(:email) }
+    it { should have_db_column(:name) }
+  end
+
+  describe '#associations' do
+    it { should have_many(:posts).dependent(:destroy) }
   end
 end
